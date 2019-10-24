4fae225d7a5c1c9c3a8dbde5feea58b0333db185
Jake Navarro <jt10navarro@gmail.com>
2019-10-24 11:04:13 -0500

    created Superhero class.  added member variables

---
+++ Superhero Creator/Models/Superhero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Superhero_Creator.Models
+{
+    public class Superhero
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string alterEgo { get; set; }
+        public string primaryAbility { get; set; }
+        public string secondaryAbility { get; set;  }
+        public string catchphrase { get; set; }
+
+    }
+}
